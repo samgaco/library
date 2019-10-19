bc5487c0910d7071e69eca58ed297a18912f4ef6
wathigo <wathigosimon@gmail.com>
2019-10-19 00:47:02 -0400

    Fix linter errors

--- javascripts/book.js
+++ javascripts/book.js
@@ -1,19 +1,18 @@
-
 let myLibrary = [];
-​
+
 const table = document.querySelector('.table');
-​
+
 const creatNode = (el) => document.createElement(el);
-​
+
 const append = (parent, child) => parent.appendChild(child);
-​
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
-​
+
 const toggleStatus = (event, index, libraryArg) => {
   const bookStatus = event.target;
   if (bookStatus.textContent === 'Read') {
@@ -24,21 +23,21 @@ const toggleStatus = (event, index, libraryArg) => {
     libraryArg[index].status = true;
   }
 };
-​
+
 const removeBook = (evt, index, libraryArg) => {
   libraryArg.splice(index, 1);
   document.querySelectorAll('.book').forEach((node) => {
     table.removeChild(node);
   });
 };
-​
+
 function addBookToLibrary(libraryArg, book) {
   document.querySelectorAll('.book').forEach((node) => {
     table.removeChild(node);
   });
   return libraryArg.concat(book);
 }
-​
+
 const startApp = () => {
   document.querySelector('.submit').addEventListener('click', (evt) => {
     submitForm(evt);
@@ -52,7 +51,7 @@ const startApp = () => {
 };
 let appStarted = false;
 const render = () => {
-​
+
   myLibrary.forEach((book, index, libraryArg) => {
     const bookElement = creatNode('div');
     const bookTitle = creatNode('h2');
@@ -60,7 +59,7 @@ const render = () => {
     const bookPages = creatNode('p');
     const bookRemove = creatNode('button');
     const bookStatus = creatNode('button');
-​
+
     bookTitle.textContent = `Title: ${book.title}`;
     bookAuthor.textContent = `Author: ${book.author}`;
     bookPages.textContent = `No of Pages: ${book.pages}`;
@@ -74,18 +73,18 @@ const render = () => {
       'bg-secondary',
       'text-white',
     );
-​
+
     append(bookElement, bookTitle);
     append(bookElement, bookAuthor);
     append(bookElement, bookPages);
     append(bookElement, bookStatus);
     append(bookElement, bookRemove);
-​
+
     bookStatus.classList.add('btn', 'btn-light');
     bookRemove.classList.add('btn', 'btn-danger');
-​
+
     append(table, bookElement);
-​
+
     bookStatus.addEventListener('click', (evt) => {
       toggleStatus(evt, bookElement.dataset.position, libraryArg);
     });
@@ -98,11 +97,10 @@ const render = () => {
     startApp();
     appStarted = true;
   }
-​
+
 };
 const submitForm = (evt) => {
   evt.preventDefault();
-  console.log('submitted!')
   const title = document.querySelector('.title').value;
   const author = document.querySelector('.author').value;
   const pages = document.querySelector('.pages').value;
@@ -111,15 +109,15 @@ const submitForm = (evt) => {
   myLibrary = addBookToLibrary(myLibrary, book);
   render();
 };
-​
+
 function openForm() {
   document.getElementById('myForm').style.display = 'block';
 }
-​
+
 function closeForm() {
   document.getElementById('myForm').style.display = 'none';
 }
-​
+
 (function main() {
   const book0 = new Book('Lord of the rings', 'tolkien', 500, false);
   const book1 = new Book(
@@ -129,7 +127,7 @@ function closeForm() {
     false,
   );
   const book2 = new Book('farwell to arms', 'ernest hemminway', 350, false);
-​
+
   myLibrary = addBookToLibrary(myLibrary, book0);
   myLibrary = addBookToLibrary(myLibrary, book1);
   myLibrary = addBookToLibrary(myLibrary, book2);
