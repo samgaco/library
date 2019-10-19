8854b9fc6e45b5925a801d7c35939dc10d4741a4
samgaco <34820784+samgaco@users.noreply.github.com>
2019-10-19 17:54:08 +0200

    Revert "Library"

--- javascripts/book.js
+++ javascripts/book.js
@@ -1,132 +1,79 @@
-/* eslint-env browser */
-/* eslint no-unused-vars: [1, {"argsIgnorePattern": "evt"}] */
-
-let myLibrary = [];
-
-const table = document.querySelector('.table');
-
-const creatNode = el => document.createElement(el);
-
-const append = (parent, child) => parent.appendChild(child);
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-const toggleStatus = (event, index, libraryArg) => {
-  const bookStatus = event.target;
-  if (bookStatus.textContent === 'Read') {
-    bookStatus.textContent = 'Not Read';
-    libraryArg[index].status = false;
-  } else {
-    bookStatus.textContent = 'Read';
-    libraryArg[index].status = true;
-  }
-};
-
-const removeBook = (evt, index, libraryArg) => {
-  libraryArg.splice(index, 1);
-  document.querySelectorAll('.book').forEach((node) => {
-    table.removeChild(node);
-  });
-};
-
-function addBookToLibrary(libraryArg, book) {
-  document.querySelectorAll('.book').forEach((node) => {
-    table.removeChild(node);
-  });
-  return libraryArg.concat(book);
-}
-
-const submitForm = (evt, renderArg) => {
-  evt.preventDefault();
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  const pages = document.querySelector('.pages').value;
-  const status = !document.querySelector('.not-read').checked;
-  const book = new Book(title, author, pages, status);
-  myLibrary = addBookToLibrary(myLibrary, book);
-  renderArg();
-};
+let myLibrary = [
+];
 
 function openForm() {
-  document.getElementById('myForm').style.display = 'block';
-}
-
+    document.getElementById("myForm").style.display = "block";
+  }
+  
 function closeForm() {
-  document.getElementById('myForm').style.display = 'none';
-}
+    document.getElementById("myForm").style.display = "none";
+  }
 
-const startApp = (renderArg) => {
-  document.querySelector('.submit').addEventListener('click', (evt) => {
-    submitForm(evt, renderArg);
-  });
-  document.querySelector('.add-book').addEventListener('click', (evt) => {
-    openForm();
-  });
-  document.querySelector('.close-form').addEventListener('click', (evt) => {
-    closeForm();
-  });
-};
+const addBook = document.querySelector('.add-book');
 
-let appStarted = false;
+const table = document.querySelector('.table')
 
-const render = () => {
-  myLibrary.forEach((book, index, libraryArg) => {
-    const bookElement = creatNode('div');
-    const bookTitle = creatNode('h2');
-    const bookAuthor = creatNode('p');
-    const bookPages = creatNode('p');
-    const bookRemove = creatNode('button');
-    const bookStatus = creatNode('button');
+const render = (evt) => {
+  myLibrary.forEach( (book, index, myLibrary) => {
+
+    let bookElement = creatNode('div');
+    let bookTitle = creatNode('h2');
+    let bookAuthor = creatNode('p');
+    let bookPages = creatNode('p');
+    let bookRemove = creatNode('button');
 
     bookTitle.textContent = `Title: ${book.title}`;
     bookAuthor.textContent = `Author: ${book.author}`;
     bookPages.textContent = `No of Pages: ${book.pages}`;
-    bookRemove.textContent = 'Remove book';
-    bookStatus.textContent = book.read ? 'Read' : 'Not read';
-    bookElement.dataset.position = `${index}`;
-    bookElement.classList.add(
-      'book',
-      'card',
-      'col-3',
-      'bg-secondary',
-      'text-white',
-    );
+    bookRemove.textContent = `Remove book`
+    bookElement.data.setAttribute(`data`, `position`: `${index}` ) 
 
     append(bookElement, bookTitle);
     append(bookElement, bookAuthor);
     append(bookElement, bookPages);
-    append(bookElement, bookStatus);
-    append(bookElement, bookRemove);
-
-    bookStatus.classList.add('btn', 'btn-light');
-    bookRemove.classList.add('btn', 'btn-danger');
+    append(bookElement, bookRemove );
 
     append(table, bookElement);
+  })
+}
 
-    bookStatus.addEventListener('click', (evt) => {
-      toggleStatus(evt, bookElement.dataset.position, libraryArg);
-    });
-    bookRemove.addEventListener('click', (evt) => {
-      removeBook(evt, bookElement.dataset.position, libraryArg);
-      render();
-    });
-  });
-  if (!appStarted) {
-    startApp(render);
-    appStarted = true;
-  }
+const creatNode = (el) => {
+  return document.createElement(el);
 };
-(function main() {
-  render();
-  const book0 = new Book('The Lord of the Rings', 'J. R. R. Tolkien', 500, false);
-  const book1 = new Book('A Confederacy of Dunces', 'John Kennedy Toole', 600, false);
-  const book2 = new Book('A Farewell to Arms', 'Ernest Hemingway', 350, false);
-  myLibrary = addBookToLibrary(myLibrary, book0);
-  myLibrary = addBookToLibrary(myLibrary, book1);
-  myLibrary = addBookToLibrary(myLibrary, book2);
-}());
+
+const append = (parent, child) => {
+  return parent.appendChild(child);
+};
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+
+    this.read = function (read) {
+        return read ? "read already" : "not read yet"
+    }
+
+    this.sayName = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read()}`
+    }
+}
+
+function addBookToLibrary(myLibrary, book) {
+    return myLibrary.concat(book);
+
+}
+
+(
+    function main() {
+        let book0 = new Book("gfda", "James", 110, false);
+        let book1 = new Book("hobbit", "tolkien", 200, false);
+        let book2 = new Book("grown cubicles", "john green", 840, false);
+
+        myLibrary = addBookToLibrary(myLibrary, book0);
+        myLibrary = addBookToLibrary(myLibrary, book1);
+        myLibrary = addBookToLibrary(myLibrary, book2);
+
+        console.log(myLibrary)
+    }
+)();
